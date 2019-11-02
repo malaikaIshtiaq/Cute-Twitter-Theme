897088cbaf731a7a3eaa7a718a6144cfb7698902
Malaika Ishtiaq <malaikamic@gmail.com>
2019-11-02 23:59:31 +0500

    Add files via upload

---
+++ content.js
@@ -0,0 +1,39 @@
+//Side Nav
+//Home Icon Instead of SVG
+var span1 = document.createElement("span1");
+document.body.appendChild(span1);
+span1.id = "home-icon";
+//Explore Icon Instead of SVG
+var span2 = document.createElement("span2");
+document.body.appendChild(span2);
+span2.id = "explore-icon";
+//Notification Icon Instead of SVG
+var span3 = document.createElement("span3");
+document.body.appendChild(span3);
+span3.id = "notify-icon";
+//Message Icon Instead of SVG
+var span4 = document.createElement("span4");
+document.body.appendChild(span4);
+span4.id = "messages-icon";
+//Bookmark Icon Instead of SVG
+var span5 = document.createElement("span5");
+document.body.appendChild(span5);
+span5.id = "bookmark-icon";
+//List Icon Instead of SVG
+var span6 = document.createElement("span6");
+document.body.appendChild(span6);
+span6.id = "list-icon";
+//Profile Icon along photo
+var span7 = document.createElement("span7");
+document.body.appendChild(span7);
+span7.id = "profile-icon";
+//More Icon Instead of SVG
+var span8 = document.createElement("span8");
+document.body.appendChild(span8);
+span8.id = "more-icon";
+
+//Profile
+//Spark Icon Replacing with unicorn
+var span9 = document.createElement("span9");
+document.body.appendChild(span9);
+span9.id = "back-icon";
